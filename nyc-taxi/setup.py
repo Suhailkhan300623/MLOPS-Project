e2c96ed055832cf746ca03bf088656211f6ce4a8
Suhail Khan <dbi.suhail@gmail.com>
2024-08-24 15:04:02 +0530

    initial commit

---
+++ nyc-taxi/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a machine learning project to predict the fair prices of new york taxi.',
+    author='Suhail Khan',
+    license='MIT',
+)
